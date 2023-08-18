1c629a694ae12a987ce49d88e923ac95099ec49d
brianleach11 <bridog314@gmail.com>
2023-08-18 02:12:11 -0400

    Working PWA Service worker registered and activated.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+}
 
-module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    register: "true",
+    skipWaiting: "true",
+    disable: process.env.NODE_ENV === 'development'
+});
+
+module.exports = withPWA(nextConfig)
