b06891b7caabfe080192489940c215ef5c1cffe9
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2023-08-31 15:30:18 -0400

    Supabase/login (#21)
    * Simple login but not working
    
    * 'Working' Login page

---
+++ src/components/Login.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import UserAuthForm from '@/components/UserAuthForm'
+import CredentialsForm from "./CredentialsForm";
+import Link from 'next/link'
+import {Button} from '@/components/ui/button'
+
+const Login = () => {
+  return (
+    <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px] border-2 border-midnight border-opacity-100 rounded-lg'>
+      <div className='flex flex-col space-y-2 text-center'>
+        <Image src="/assets/logo.png" width={75} height={75} alt="Logo" className='mx-auto'/>
+        <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
+        <p className='text-sm max-w-xs mx-auto'>
+          Sign in to re-connect with your community!
+        </p>
+      </div>
+      <CredentialsForm/>
+      <div className="text-center font-bold">
+        Or
+      </div>
+      <div className="text-center font-bold">
+        Sign up with Google!
+      </div>
+      <UserAuthForm />
+      <p className='px-8 text-center text-sm text-muted-foreground py-5'>
+        New to Pet Connect?{' '}
+        <Link
+          href='/sign-up'
+          className='hover:text-brand text-sm underline underline-offset-4'>
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  )
+}
+
+export default Login
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,27 @@
 import {NextAuthOptions} from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const authConfig: NextAuthOptions ={
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+              email: {
+                label: "Email",
+                type: "email",
+                placeholder: "example@example.com",
+              },
+              password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+              const user = { id: "1", name: "Admin", email: "admin@admin.com" };
+              return user;
+            },
         }),
     ],
 }
