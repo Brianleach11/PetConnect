e41e21c1a278b0340050eec0a2712bc42dff74cb
brianleach11 <bridog314@gmail.com>
2023-09-01 14:57:05 -0400

    Simple Sign Up page, login works w google

---
+++ src/components/SingUp.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import Link from 'next/link'
+import SignUpForm from '@/components/SignUpForm'
+
+const SignUp = () => {
+  return (
+    <div className='container py-4 mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px] bg-whiteGreen border-2 border-midnight border-opacity-100 rounded-lg'>
+      <Link href="/" className="text-left hover:text-brand text-sm underline underline-offset-4"> Home</Link>
+      <div className='flex flex-col space-y-2 text-center'>
+        <Image src="/assets/logo.png" width={75} height={75} alt="Logo" className='mx-auto'/>
+        <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
+        <p className='text-sm max-w-xs mx-auto'>
+          Sign in to re-connect with your community!
+        </p>
+      </div>
+      <SignUpForm/>
+    </div>
+  )
+}
+
+export default SignUp
