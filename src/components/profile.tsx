92f13d3118b30e75f256c7924d339a9d271d4d8d
aba98s <abanoub.mz10@gmail.com>
2023-09-22 23:51:18 -0400

    profile

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -5,8 +5,6 @@ import { Database } from '@/types/supabase';
 import moment from 'moment';
 import { Card, CardHeader, CardContent } from '@/components/ui/card';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
-import Image from "next/image";
-
 
 const Profile: React.FC = () => {
   const [userData, setUserData] = useState<Database['public']['Tables']['user']['Row'] | null>(null);
@@ -32,67 +30,76 @@ const Profile: React.FC = () => {
 
 
   return (
-    <div className="w-[1000px] mx-auto mt-100 space-y-4">
-      {/* Pet Profile Card */}
-      <Card className="white rounded-lg">
-        <CardHeader className="bg-white text-black flex items-center justify-between p-4 rounded-t-lg">
-          <div className="flex items-center">
-            <Avatar>
-              <AvatarImage src="/assets/logo.png" alt="Logo" />
-            </Avatar>
-            <h2 className="text-lg">{petData?.name || 'Pet Name'}</h2>
+    <div className="bg-gray-100 bg-opacity-25 lg:w-8/12 lg:mx-auto mb-8">
+      <header className="flex flex-wrap items-center p-4 md:py-8">
+        <div className="md:w-3/12 md:ml-16">
+        <img 
+          className="w-20 h-20 md:w-40 md:h-40 object-cover rounded-full border-2 border-pink-600 p-1" 
+          src="https://images.unsplash.com/photo-1561948955-570b270e7c36?fit=crop&w=500&h=500" 
+          alt="Dog Image" 
+          width={160} 
+         height={160} 
+     />
+        </div>
+        <div className="w-8/12 md:w-7/12 ml-4">
+          <div className="md:flex md:flex-wrap md:items-center mb-4">
+            <h2 className="text-3xl inline-block font-light md:mr-2 mb-2 sm:mb-0">
+              {userData?.username || 'Username'}
+            </h2>
           </div>
-        </CardHeader>
-        <CardContent className="p-4">
-          {petData ? (
-            <div>
-              <p><strong>Type:</strong> {petData.pet_type}</p>
-              <p><strong>Breed:</strong> {petData.breed}</p>
-              <p><strong>Sex:</strong> {petData.sex}</p>
-              <p><strong>Birthday:</strong> {petData.birthday}</p>
-              <p><strong>Age:</strong> {petData.birthday}</p>
-              <hr />
-              <div className="mt-4">
-                <h3 className="text-lg font-semibold">Bio:</h3>
-                <p className="text-justify">{petData.bio}</p>
-              </div>
+          <div className="hidden md:block">
+            <h1 className="font-semibold">My name is {petData?.name || 'Pet Name'} :)</h1>
+            <span>Iam a {petData?.pet_type}, My breed is {petData?.breed}</span>
+            <p>I was born on {`${petData?.birthday}`}</p>
+            <p> {userData?.city}, {userData?.state}`</p>
+          </div>
+        </div>
+      </header>
+      <div className="px-px md:px-3 mt-4">
+        <Card>
+          <CardHeader>
+            <h3 className="text-xl font-semibold">Bio</h3>
+          </CardHeader>
+          <CardContent>
+            <p className="text-gray-600">{petData?.bio || 'This user has no bio.'}</p>
+          </CardContent>
+        </Card>  
+      </div>
+      <div className="px-px md:px-3 mt-4">
+        {/* Attachments Section */}
+        <Card>
+          <CardHeader>
+            <h3 className="text-xl font-semibold">Medication Documents </h3>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-wrap">
+              
             </div>
-          ) : (
-            <p>Loading...</p>
-          )}
-        </CardContent>
-      </Card>
-
-      {/* User Profile Card */}
-      <Card className="white rounded-lg">
-        <CardHeader className="bg-white text-black p-4 rounded-t-lg">
-          <h2 className="text-lg">User Profile</h2>
-        </CardHeader>
-        <CardContent className="p-4">
-          {userData ? (
-            <div>
-              <p><strong>Username:</strong> {userData.username}</p>
-              <p><strong>Gender:</strong> {userData.gender}</p>
-              <p><strong>Birthday:</strong> {userData.birthday}</p>
-              <p><strong>City:</strong> {userData.city}</p>
-              <p><strong>State:</strong> {userData.state}</p>
-              <p><strong>Iam looking for:</strong> {userData.looking_for}</p>
-            </div>
-          ) : (
-            <p>Loading...</p>
-          )}
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+      </div>
       
-      <Card className="white rounded-lg">
-        <CardHeader className="bg-white text-black flex items-center justify-between p-4 rounded-t-lg">
-          <div className="flex items-center">
-            <h2 className="text-lg">Medication and Vaccination</h2>
-          </div>
-        </CardHeader>
-      </Card>
+      <div className="px-px md:px-3 mt-4">
+        <hr className="my-4"/> {/* Horizontal line */}
+        <div className="flex flex-wrap -mx-px md:-mx-3">
+          {/* The code for displaying user posts with images */}
+          {[...Array(6)].map((_, index) => (
+            <div className="w-1/2 md:w-1/3 p-px md:p-3 flex justify-center items-center" key={index}>
+              <img 
+                style={{ maxWidth: "300px", maxHeight: "300px" }}
+                className="w-full h-full object-cover border-2 border-pink-600 p-1" 
+                src="https://images.unsplash.com/photo-1561948955-570b270e7c36?fit=crop&w=500&h=500" 
+                alt="Dog Image" 
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
+
+    
+  
 };
 
 export default Profile;
