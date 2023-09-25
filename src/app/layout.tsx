4c974740810a32d5ad06100559bae21da9bcf99e
brianleach11 <bridog314@gmail.com>
2023-09-24 20:41:04 -0400

    Removed async from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   manifest: '/manifest.json'
 }
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
