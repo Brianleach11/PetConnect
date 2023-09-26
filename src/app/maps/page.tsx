cbdd0f5e48cac513c6bb86de4be88d195010ce42
brianleach11 <bridog314@gmail.com>
2023-09-26 13:10:05 -0400

    Get coordinates and delete unused code

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,77 +1,41 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import NavBar from '@/components/NavBar'
-export const dynamic = 'force-dynamic'
-import mapboxgl from "mapbox-gl";
-import page from '../(auth)/login/page'
 import MapboxMap from '@/components/map/MapboxMap'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/types/supabase';
-import { NextResponse } from 'next/server'
+import { redirect } from 'next/navigation';
+//export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
-  const cookieStore = cookies().getAll()
-  const regex = /.*?(?=auth-token-code-verifier)/;
-  const authToken = cookieStore.some(cookie => regex.test(cookie.name));
+  var city: string | null = null;
+  var state: string | null = null;
 
   const{
     data: { session },
+    error: sessionError
   } = await supabase.auth.getSession()
 
-  const {data: {user}, error} = await supabase.auth.getUser()
+  if(!session || sessionError) redirect('/')
 
-  var city;
-  var state;
+  const {data: userData, error: userError} = await supabase
+  .from("user")
+  .select("city,state")
+  .eq('id', session.user.id)
+  .single();
 
-  if(user?.id && !error) {
-    const {data, error} = await supabase
-    .from("user")
-    .select("city,state")
-    .eq('id', user?.id);
-    city = data?.at(0)?.city
-    state = data?.at(0)?.state
+  if(userData && !userError){
+    city = userData.city
+    state = userData.state
   }
-
-  var url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + {city} + " " + {state} + ".json";
-  //https://api.mapbox.com/geocoding/v5/mapbox.places/hollywood%20florida.json?access_token=pk.eyJ1IjoicGV0LWNvbm5lY3QiLCJhIjoiY2xtZmswYm5pMDFuazNsbWV1ampseHl6YiJ9.NnNxP18_d4ankhK-woxOWA  
-  //https://api.mapbox.com/geocoding/v5/mapbox.places/?query={}.json?&access_token=${process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
-  //const response = await fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/hollywood%20florida.json?access_token=pk.eyJ1IjoicGV0LWNvbm5lY3QiLCJhIjoiY2xtZmswYm5pMDFuazNsbWV1ampseHl6YiJ9.NnNxP18_d4ankhK-woxOWA');
-  var query = city + " " + state;
-  
-  //const stuff = GET();
-  
-
-  //GET() fetch here
-  async function GET() {
-    const res = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=1&autocomplete=false&access_token=${process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}`)
-    const data = await res.json().then((data) => {
-      console.log(data)
-    })
-    //sigh. it'd be nice if we could just use the API for the forward geocoding
-    //then get the coordinates from the fetched data
-    //but does this GET() before the page loads? 
-    //since the function in the page.tsx is also a async maybe we can call the function in there...
-    
-    return NextResponse.json({ data })
-}
-  //const location = GET();
-  //console.log(location);
-
-
-
-  //parse results
-
   return (
     <>
-      <NavBar session={session} authToken={authToken}/>
-      <div>
+      <NavBar session={session} authToken={false}/>
+      <div className='py-12'>
         <div>Welcome to the maps page!</div>
         <div>Explore other pet owners in your area, as well as pet-centered locations like parks, pet groomers, and more!</div>
         <div className="relative center flex-auto w-full">
-          <MapboxMap session={session} city={city ? city:"Davie"} state={state? state:"Florida"}/>
+          <MapboxMap city={city ? city:"Davie"} state={state ? state:"Florida"}/>
         </div>
-        
       </div>
     </>
   )
--- src/components/map/MapboxMap.tsx
+++ src/components/map/MapboxMap.tsx
@@ -1,96 +1,53 @@
 'use client'
-import {useRouter} from 'next/navigation'
-import { useEffect, useRef } from "react";
+import { useEffect, FC, useState } from "react";
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { Map } from 'react-map-gl'
-import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '@/types/supabase';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import mapboxgl from 'mapbox-gl';
-import React, { CSSProperties } from 'react';
 import mapboxSdk from '@mapbox/mapbox-sdk/services/geocoding'
-import { get } from 'http';
 
-export default function MapboxMap({session, city, state}: {session: Session | null, city: string, state: string}) {
+interface MapboxMapProps{
+  city: string,
+  state: string,
+}
+
+const MapboxMap: FC<MapboxMapProps> = ({city, state}) => {
   //const mapboxClient = require('@mapbox/mapbox-sdk');
   const geocodingClient = mapboxSdk({ accessToken: process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN ?? ""});
-  
-  function getLocation(_coordinates: number[]) {
-    var long;
-    var latt;
-    geocodingClient.forwardGeocode({
-      query: city + " " + state,
-      countries: ['us'], // Limit results to United States
-      autocomplete: false,
-      limit: 1
-    }).send()
-      .then((response: { body: { features: any; }; }) => {
-        const features = response.body.features;
-        // Do something with the search results
-        long = features[0].center[0];
-        latt = features[0].center[1];
-        _coordinates[0] = features[0].center[0];
-        _coordinates[1] = features[0].center[1];
-        console.log(features);
-        console.log("longitude and latitude: ", long, latt);
-      });
-      return _coordinates;
-      
-  }
+  const [center, setCenter] = useState<number[]>([])
 
-  //default center if no location
-  var lng = -96.1;
-  var lat = 39.5;
-  
-  var coord = [0,0];
-  coord = getLocation(coord);
-  console.log("Coordinates: ", coord);
+  useEffect(()=>{
+    async function getLocation() {
+      const res = await geocodingClient.forwardGeocode({
+        query: city + " " + state,
+        countries: ['us'], // Limit results to United States
+        autocomplete: false,
+        limit: 1
+      }).send()
+      setCenter(res.body.features[0].center)
+    }
+    getLocation()
+  }, [city, state])
 
-  var startViewState = {    
-    longitude: coord[0],
-    latitude: coord[1],
-    zoom: 7
-  };
-      
-  const geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl
-  });
-
-  const url = "https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json"
-
-  /*return (
-    <div id="map">   
-    </div>
-  );*/
-
-  /*
-  useEffect(() => {
-      const fetchLocations = async () => {
-        await fetch(url).then((response) =>
-          response.text()).then((res) => JSON.parse(res))
-        .then((json) => {
-          setLocations(json.features);
-        }).catch((err) => console.log({ err }));
-      };
-      fetchLocations();
-    }, []);
-*/
   return (
       <div>
+        {center.length > 0 ? 
           <Map 
               mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
               initialViewState={{    
-                longitude: coord[0],
-                latitude: coord[1],
-                zoom: 7
+                longitude: center[0],
+                latitude: center[1],
+                zoom: 10
               }}
               style={{width: 800, height: 500}}
               
               mapStyle="mapbox://styles/mapbox/streets-v12"
           />
+          :
+          <div className="text-lg text-midnight"> Loading...</div>
+          }
       </div>
       
     );
 }
+
+export default MapboxMap
