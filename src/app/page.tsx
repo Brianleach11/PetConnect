1fc1f4f7894ba8e02f8a385648faa9fd15dd0e7a
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2023-10-17 18:15:06 -0400

    fixed refresh on login, working on homepg w/o auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import NavBar from '@/components/NavBar';
 import PetCardList from '@/components/PetCardList';
+import SimpleNav from '@/components/SimpleNav';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -11,17 +12,27 @@ export default async function Home() {
 
   const {
     data: { session },
+    error
   } = await supabase.auth.getSession();
 
   return (
-    <div className="flex flex-col min-h-screen bg-whiteGreen">
-      <NavBar session={session} authToken={authToken} />
-      <h1 className="font-fredokaOne text-3xl text-center mt-12 mb-8 text-darkBlue z-10">
-        Time to connect with others!
-      </h1>
-      <div className="flex-grow">
-        <PetCardList />
+    session ? (
+      <div className="flex flex-col min-h-screen bg-whiteGreen">
+        <NavBar session={session} />
+        <h1 className="font-fredokaOne text-3xl text-center mt-12 mb-8 text-darkBlue z-10">
+          Time to connect with others!
+        </h1>
+        <div className="flex-grow">
+          <PetCardList />
+        </div>
       </div>
+    )
+    :
+    <div className='font-bold pt-12'>
+      <h1>
+      NOT LOGGED IN
+      </h1>
+      <SimpleNav session={session} authToken={authToken}/>
     </div>
   );
 }
