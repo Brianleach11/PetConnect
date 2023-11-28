c25311aa88a303563ed5e9b69e7fbc551f900ccd
aba98s <abanoub.mz10@gmail.com>
2023-11-27 21:17:11 -0500

    fix card layout

--- src/components/OwnerPetCardList.tsx
+++ src/components/OwnerPetCardList.tsx
@@ -44,7 +44,7 @@ const OwnerPetCardList: React.FC<Props> = ({ userId }) => {
 
   return (
     <div className="flex justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full md:max-w-screen">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full md:max-w-screen">
         {pets.map(pet => (
           <div key={pet.id} className="overflow-hidden">
             <PetCard pet={pet} />
